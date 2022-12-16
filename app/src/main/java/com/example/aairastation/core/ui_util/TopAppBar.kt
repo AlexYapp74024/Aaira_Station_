b6ee5b33eb34dc8aef3d16699beae56fe0218a3d
AlexYapp74024 <95156272+AlexYapp74024@users.noreply.github.com>
2022-12-16 20:55:24 +0800

    Renamed Function

--- app/src/main/java/com/example/aairastation/core/ui_util/TopAppBar.kt
+++ app/src/main/java/com/example/aairastation/core/ui_util/TopAppBar.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ForageTopAppBar(
+fun DefaultTopAppBar(
     Title: String,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
