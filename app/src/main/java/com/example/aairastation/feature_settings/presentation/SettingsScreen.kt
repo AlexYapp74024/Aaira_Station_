e80d5105d4a02eedac4cda2dc2fc68496089eb78
AlexYapp74024 <95156272+AlexYapp74024@users.noreply.github.com>
2022-12-26 15:42:02 +0800

    Basic foundation for settings screem

--- app/src/main/java/com/example/aairastation/feature_settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/example/aairastation/feature_settings/presentation/SettingsScreen.kt
@@ -1,10 +1,49 @@
 package com.example.aairastation.feature_settings.presentation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.aairastation.core.ui_util.BottomNavItems
+import com.example.aairastation.core.ui_util.DefaultBottomNavigation
+import com.example.aairastation.core.ui_util.DefaultTopAppBar
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+private var navigator: DestinationsNavigator = EmptyDestinationsNavigator
 
 @Destination
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(navigatorIn: DestinationsNavigator) {
+    navigator = navigatorIn
 
+    SettingsScaffold {
+        SettingsScreenContent()
+    }
+}
+
+@Composable
+fun SettingsScreenContent() {
+
+}
+
+@Composable
+fun SettingsScaffold(
+    modifier: Modifier = Modifier,
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            DefaultTopAppBar(title = "Settings", canNavigateBack = false, navigateUp = {})
+        },
+        bottomBar = {
+            DefaultBottomNavigation(
+                currentItem = BottomNavItems.Settings,
+                navigator = navigator
+            )
+        },
+        modifier = modifier,
+        content = content,
+    )
 }
